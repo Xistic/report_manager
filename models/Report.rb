ecf1243ca26366afa9dff1af3dd1d1111cfa5497
egor pushkarev <egorka.pushkarev.02@mail.ru>
2024-04-19 00:54:41 +0300

    Настроил вывод отчета в терминал

--- models/Report.rb
+++ models/Report.rb
@@ -3,8 +3,13 @@ class Report
     attr_accessor :type, :date_deadline, :days_to_deadline
   
     def initialize(type, date_deadline, days_to_deadline)
+        @type = type
         @date_deadline = date_deadline
         @days_to_deadline = days_to_deadline
+    end
+
+    def to_s
+        "Type: #{@type}, Date Deadline: #{date_deadline}, Days to Deadline: #{days_to_deadline}"
     end
 
 end
