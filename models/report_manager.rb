4d7907c45c13053296270d29e670c3187a14c5b2
egor pushkarev <egorka.pushkarev.02@mail.ru>
2024-04-19 00:52:55 +0300

    Пофиксил регистр типа для передачи в функции. Убрал load_report, т.к. настроил вывод у Report

--- models/report_manager.rb
+++ models/report_manager.rb
@@ -6,19 +6,21 @@ class ReportManager
   def initialize
     @time_machine = ReportTimeMachine.new
     @report_files = {
-      monthly: 'monthly_report.json',
-      quarterly_10: 'quarterly_report_10.json',
-      quarterly_30: 'quarterly_report_30.json',
-      annual_10: 'annual_report_10.json',
-      annual_30: 'annual_report_30.json'
+      monthly: 'storage_reports/monthly_report.json',
+      quarterly_10: 'storage_reports/quarterly_report_10.json',
+      quarterly_30: 'storage_reports/quarterly_report_30.json',
+      annual_10: 'storage_reports/annual_report_10.json',
+      annual_30: 'storage_reports/annual_report_30.json'
     }
   end
 
   def add_report(type)
-    date_deadline = @time_machine.deadline_date(type)
-    days_to_deadline = @time_machine.days_to_deadline(type)
+    date_deadline = @time_machine.deadline_date(type.downcase.gsub(' ', '_'))
+    days_to_deadline = @time_machine.days_to_deadline(type.downcase.gsub(' ', '_'))
 
     report = Report.new(type, date_deadline, days_to_deadline)
+
+    puts report
 
     report_file = @report_files[type.downcase.gsub(' ', '_').to_sym]
     File.open(report_file, 'w') do |file|
@@ -26,8 +28,4 @@ class ReportManager
     end
   end
 
-  def load_report(type)
-    report_file = @report_files[type.downcase.gsub(' ', '_').to_sym]
-    JSON.parse(File.read(report_file))
-  end
 end
