a883e5fa80909e1068fd4f69e095ce0e1dde75dd
egor pushkarev <egorka.pushkarev.02@mail.ru>
2024-04-19 16:08:20 +0300

    Заглушка

--- main.rb
+++ main.rb
@@ -1,10 +1,16 @@
-require_relative 'models/report_manager.rb'
+# require_relative 'controller/report_manager.rb'
+require_relative 'view/report_menu.rb'
 
-manager = ReportManager.new
+# manager = ReportManager.new
 
-manager.add_report('Monthly')
-manager.add_report('Quarterly 10')
-manager.add_report('Quarterly 30')
-manager.add_report('Annual 10')
-manager.add_report('Annual 30')
+# manager.get_report('Monthly')
+# manager.get_report('Quarterly 10')
+# manager.get_report('Quarterly 30')
+# manager.get_report('Annual 10')
+# manager.get_report('Annual 30')
+# manager.get_report ('Annual 30')
+
+report_menu_tool = ReportMenu.new
+
+report_menu_tool.get_next_report
 
