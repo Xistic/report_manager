ba7cda7acd9310d58243c9610e15dc3168d4503c
egor pushkarev <egorka.pushkarev.02@mail.ru>
2024-04-20 15:52:12 +0300

    Инкапсуляция контроллера

--- view/report_menu.rb
+++ view/report_menu.rb
@@ -1,18 +1,37 @@
 require_relative '../controller/report_manager'
 
 class ReportMenu
-
+    
     def initialize
-        @report_manager = ReportManager.new
+        initialize_report_manager
+    end
+    
+    def get_next_report
+        update_report
+        reports_filter
+        
     end
 
-    def get_next_report
-        @report_manager.update_report
-        @report_manager.reports_filter
-    end 
-
     def refresh_folder
+        reload_folder
+    end
+  
+    private
+    
+    def initialize_report_manager
+        @report_manager = ReportManager.new
+    end
+  
+    def update_report
+        @report_manager.update_report
+    end
+  
+    def reports_filter
+        @report_manager.reports_filter
+    end
+  
+    def reload_folder
         @report_manager.reload_folder
-    end 
+    end
 
-end 
+  end 
