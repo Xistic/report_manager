8bd8d7937a3c93045a301ff5fbd483d8ac6adf69
egor pushkarev <egorka.pushkarev.02@mail.ru>
2024-04-22 13:00:43 +0300

    Отредачил фильтр

--- controller/report_manager.rb
+++ controller/report_manager.rb
@@ -27,14 +27,12 @@ class ReportManager
     min_days_report = []
     @report_files.each do |type, file_path|
       report_data = load_report_data(file_path)
-      if report_data
-        if report_data['Days-to-Deadline'] <= min_days_to_deadline
-          if report_data['Days-to-Deadline'] < min_days_to_deadline
-            min_days_report = []
-            min_days_to_deadline = report_data['Days-to-Deadline']
-          end
-          min_days_report << report_data
+      if report_data['Days-to-Deadline'] <= min_days_to_deadline
+        if report_data['Days-to-Deadline'] < min_days_to_deadline
+          min_days_report = []
+          min_days_to_deadline = report_data['Days-to-Deadline']
         end
+          min_days_report << report_data
       end
     end
     puts min_days_report
